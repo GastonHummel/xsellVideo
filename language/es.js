ec5bdd6e0cbd5f27be270771e3bcd3ea77edceb7
Gaston Hummel <gaston.hummel@pb.com>
2020-03-26 13:15:58 +0100

    first commit

---
+++ language/es.js
@@ -0,0 +1,58 @@
+var Translations = {
+    "SkipBackButtonText": "Atrás",
+    "SkipForwardButtonText": "Avanzar",
+    "PlayPauseButtonText": "Reproducir/Pausar",
+    "VolumeButtonText": "Volumen",
+    "SettingsButtonText": "Ajustes",
+    "ResumeVideoPlayback": "Reanudar la reproducción de vídeo",
+
+    "QualityMenuText": "Calidad",
+    "QualityAutoText": "Auto",
+    "LanguageMenuText": "Idioma",
+    "CCMenuText": "Subtítulos",
+    "BackFromText": "Back from",
+    "OnText": "On",
+    "OffText": "Off",
+
+    "SocialIcon-facebook": "Facebook",
+    "SocialIcon-twitter": "Twitter",
+    "SocialIcon-googleplus": "Google+",
+    "SocialIcon-email": "Email",
+    "SocialIcon-link": "Website",
+    "CollapseText": "Colapso",
+    "ExpandText": "Expandir",
+
+    "PreparedForText": "Preparado para",
+
+    "CopyrightText": "Copyright " + Utils.getFullYear() + " Pitney Bowes",
+    "PoweredByFooterText": "Powered by",
+    "HelpFooterText": "Ayuda",
+    "PrivacyPolicyFooterText": "Política de privacidad",
+
+    /*
+     * See en.js for details
+     */
+    "CtaButtonSettings": {
+    },
+
+    /*
+     * See en.js for details
+     */
+    "PromoSettings": {
+    },
+
+    /*
+     * See en.js for details
+     */
+    "ChapterSettings": [
+        {
+            "label": "EngageOne Converse Introduction",
+            "states": [
+                {
+                    "cardId": "START",
+                    "width": 100
+                }
+            ]
+        }
+    ]
+}
